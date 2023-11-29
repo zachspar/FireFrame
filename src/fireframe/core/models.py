9f3c025f1a66c91d1dd92d8438de8cc51387b702
Zachary Spar <zachspar@gmail.com>
2023-11-29 01:57:58 -0500

    core library scaffold, BaseModel using FireO TypedModel

---
+++ src/fireframe/core/models.py
@@ -0,0 +1,14 @@
+from fireo.typedmodels import TypedModel
+
+
+__all__ = [
+    "BaseModel",
+]
+
+
+class BaseModel(TypedModel):
+    """
+    Base model for all FireFrame models.
+
+    Inherits from FireO's TypedModel.
+    """
