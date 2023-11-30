e04ffa31e63a0f0faef9ac42463e4bd3a8a708d5
Zachary Spar <zachspar@gmail.com>
2023-11-29 20:52:00 -0500

    add sample_app, wip

---
+++ sample_app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fireframe.core.models import *
+from fireframe.core.serializers import *
+from fireframe.core.views import *
+
+
+class User(Model):
+    """
+    User model.
+
+    Inherits from FireFrame's Model.
+    """
+
+    class Meta:
+        collection_name = "users"
+
+    name: str
+    email: str
+    age: int
+
+
+class UserSerializer(ModelSerializer):
+    """
+    User serializer.
+
+    Inherits from FireFrame's ModelSerializer.
+    """
+
+    class Meta:
+        model = User
+        fields = ["name", "email", "age"]
+
+
+class UserGetAPI(BaseReadAPIView):
+    serializer_class = UserSerializer
+
+
+app = FastAPI(title="Sample FireFrame App", version="0.0.0")
+app.include_router(UserGetAPI.as_router())
